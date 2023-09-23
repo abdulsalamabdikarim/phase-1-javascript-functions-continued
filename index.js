70e55bb8b508907fd3d05c1b5e0e2afe33267801
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-09-23 15:42:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//console.log(saturdayFun()); can be accessed before initialization
+//because functions declared in this way are hoisted
+function saturdayFun(activity = `roller-skate`){
+    return `This Saturday, I want to ${activity}!`;
+}
+//console.log(mondayWork()); cannot be accessed before initialization
+//because it is a function expression. It is not hoisted
+const mondayWork = function(activity = `go to the office`){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = `*`){
+    return function(praise = `special`){
+        return `You are ${visualFlair}${praise}${visualFlair}!`;
+    }
+}
+
+//console.log(wrapAdjective(`%%`)(`awesome`));
